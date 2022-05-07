b5c1df896eff3a046d32eb0e5ebb1ecf4d479027
NAGARRO\shwetasrivastava <shweta.srivastava@nagarro.com>
2022-05-07 19:51:31 +0530

    enhancements

--- src/components/Lane.js
+++ src/components/Lane.js
@@ -1,16 +1,16 @@
 import './Lane.css';
-import React, { useEffect, useRef } from 'react'
+import React, { memo, useEffect, useRef } from 'react'
 
 const Lane = props => {
     const participantEle = useRef();
 
     useEffect(() => {
-        let num1 = Number.parseInt(participantEle.current.style.left);
-        let num2 = participantEle.current.parentElement.clientWidth;
-        if(num1 >= num2 - 95) {
+        let fromLeftPos = Number.parseInt(participantEle.current.style.left);
+        let parentTotalWidth = participantEle.current.parentElement.clientWidth;
+        if (fromLeftPos >= parentTotalWidth - 90) {
             props.winnerFinalized()
         }
-    }, [props.fromLeft])
+    })
 
     const fromLeft = { left: props.fromLeft };
     return (
@@ -26,4 +26,4 @@ const Lane = props => {
     )
 }
 
-export default Lane;
+export default memo(Lane);
--- src/components/RaceController.js
+++ src/components/RaceController.js
@@ -6,30 +6,39 @@ const RaceController = () => {
 
     const [turtlePos, setTurtlePos] = useState(30);
     const [rabbitPos, setRabbitPos] = useState(30);
-    const [textVal, setText] = useState('PAUSE');
+    const [winnerName, setWinnerName] = useState('');
+    const [buttonText, setButtonText] = useState('PAUSE');
     const interval = useRef(null);
     const isPaused = useRef(false);
+    let winnerDiv = winnerName.length ? <div className='winner'> WINNER WINNER CHICKEN DINNER: {winnerName}</div> : '';
 
     useEffect(() => {
         return () => {
             if (interval.current) {
                 clearInterval(interval.current);
+                interval.current = null;
             }
         }
     }, [])
 
     function resetPos() {
-        setTurtlePos(30);
-        setRabbitPos(30);
-        if (interval.current) {
-            clearInterval(interval.current);
+        if (turtlePos !== 30 || rabbitPos !== 30) {
+            setWinnerName('');
+            setButtonText('PAUSE');
+            setTurtlePos(30);
+            setRabbitPos(30);
+            if (interval.current) {
+                clearInterval(interval.current);
+                interval.current = null;
+            }
         }
     }
 
     function startRace() {
-        if (!interval.current) {
+        if (!interval.current && (turtlePos === 30 || rabbitPos === 30)) {
+            setWinnerName('');
+            setButtonText('PAUSE');
             isPaused.current = false;
-            setText('PAUSE');
             keepRunning();
         }
     }
@@ -37,9 +46,9 @@ const RaceController = () => {
     function keepRunning() {
         interval.current = setInterval(() => {
             if (!isPaused.current) {
-                let randomInt = Math.floor(Math.random() * 25);
+                let randomInt = Math.floor(Math.random() * 25) + 1;
                 setTurtlePos((turtlePos) => turtlePos + randomInt);
-                randomInt = Math.floor(Math.random() * 25);
+                randomInt = Math.floor(Math.random() * 25) + 1;
                 setRabbitPos((rabbitPos) => rabbitPos + randomInt);
             }
         }, 200);
@@ -48,27 +57,28 @@ const RaceController = () => {
     function pauseRace() {
         if (interval.current) {
             isPaused.current = !isPaused.current;
-            isPaused.current ? setText('RESUME') : setText('PAUSE');
+            isPaused.current ? setButtonText('RESUME') : setButtonText('PAUSE');
         }
     }
 
     function winnerFinalized(name) {
         if (interval.current) {
-            alert('Winner is ' + name)
             clearInterval(interval.current);
             interval.current = null;
+            setWinnerName(name);
         }
     }
 
     return (
         <div className='arena'>
             <Lane startLine='ART' endLine='FIN' imoji='🐢' fromLeft={turtlePos + 'px'}
-                winnerFinalized={() => winnerFinalized('turtle')}></Lane>
+                winnerFinalized={() => winnerFinalized('TORTOISE')}></Lane>
             <Lane startLine='ST' endLine='ISH' imoji='🐇' fromLeft={rabbitPos + 'px'}
-                winnerFinalized={() => winnerFinalized('rabbit')}></Lane>
+                winnerFinalized={() => winnerFinalized('RABBIT')}></Lane>
+            {winnerDiv}
             <div className='actions'>
                 <button onClick={startRace}>GET SET GO!!</button>
-                <button onClick={pauseRace}>{textVal}</button>
+                <button onClick={pauseRace}>{buttonText}</button>
                 <button onClick={resetPos}>NEW RACE</button>
             </div>
         </div>
